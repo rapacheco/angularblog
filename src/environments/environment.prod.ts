5cb3fcc959b32b6f0b06368eb7192fe137fa82a0
Rafael Pacheco Ribeiro <rapachecobeiro@hotmail.com>
2019-03-24 19:19:51 -0400

    Remove API KEY

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBK_GxuMMc_5d64hnFkQQh5qC7fRfLeTgM",
+    apiKey: [API KEY HERE],
     authDomain: "angularblog-cee7e.firebaseapp.com",
     databaseURL: "https://angularblog-cee7e.firebaseio.com",
     projectId: "angularblog-cee7e",
     storageBucket: "angularblog-cee7e.appspot.com",
-    messagingSenderId: "603698792211"
+    messagingSenderId: [MESSAGING ID HERE]
   }
 };
